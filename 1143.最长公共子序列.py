77556203fd6a4f49e57b8837a32e4396ad3f5ea3
Vincent <andyli386@gmail.com>
2021-02-09 01:08:33 +0800

    modify 1143

--- 1143.最长公共子序列.py
+++ 1143.最长公共子序列.py
@@ -2,7 +2,7 @@
 Author: Vincent
 Date: 2021-02-05 12:11:32
 LastEditors: Vincent
-LastEditTime: 2021-02-05 13:13:16
+LastEditTime: 2021-02-08 22:50:17
 Description: file content
 '''
 #
@@ -19,31 +19,31 @@ class Solution:
         for i in range(self.count):
             res += '*'
         print(res+str(out))
-    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
-        def dp(i, j):
-            # self.count += 1
-            # self.printIndent(f'{i} {j} text1 : {text1[:i]}, text2 : {text2[:j]}')
-            if i == -1 or j == -1:
-                self.count -= 1
-                return 0
-            if text1[i] == text2[j]:
-                return dp(i-1, j-1) + 1
-            else:
-                return max(dp(i-1, j), dp(i, j-1))
-
-        return dp(len(text1)-1, len(text2)-1)
     # def longestCommonSubsequence(self, text1: str, text2: str) -> int:
-    #     m = len(text1); n = len(text2)
-    #     dp = [[0 for i in range(n+1)] for j in range(m+1)]
+    #     def dp(i, j):
+    #         # self.count += 1
+    #         # self.printIndent(f'{i} {j} text1 : {text1[:i]}, text2 : {text2[:j]}')
+    #         if i == -1 or j == -1:
+    #             self.count -= 1
+    #             return 0
+    #         if text1[i] == text2[j]:
+    #             return dp(i-1, j-1) + 1
+    #         else:
+    #             return max(dp(i-1, j), dp(i, j-1))
 
-    #     for i in range(1, m+1):
-    #         for j in range(1, n+1):
-    #             if text1[i-1] == text2[j-1]:
-    #                 dp[i][j] = dp[i-1][j-1]+1
-    #             else:
-    #                 dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+    #     return dp(len(text1)-1, len(text2)-1)
+    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        m = len(text1); n = len(text2)
+        dp = [[0 for i in range(n+1)] for j in range(m+1)]
 
-    #     return dp[m][n]
+        for i in range(1, m+1):
+            for j in range(1, n+1):
+                if text1[i-1] == text2[j-1]:
+                    dp[i][j] = dp[i-1][j-1]+1
+                else:
+                    dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+
+        return dp[m][n]
 # @lc code=end
 
 
